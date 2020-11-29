b5d09d800b4f2cb28ef34d3961ebb841f20f83a5
Michael Nnaji <mnnaji3@uwo.ca>
2020-11-29 17:01:20 +0000

    Automatically backed up by Learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,11 +1,17 @@
 class Dog
   attr_accessor :name, :breed ,:age
 
+  @@store = []
+  
   def initialize(name,breed,age)
      @name = name
      @age=age
      @breed = breed
-
+     @@store << self
   end
 
+  def self.all
+    @@store 
+  end 
+  
 end
